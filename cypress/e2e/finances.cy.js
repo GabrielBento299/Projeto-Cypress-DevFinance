0c418933c032d8e2c3443c23518dea0944bb76c4
Gabriel Bento <gabrielbento299@hotmail.com>
2023-04-19 12:19:21 -0300

    added new test with required field

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,32 +1,58 @@
 /// <reference types="cypress" />
 
-describe('Transações', () => {
-
+describe('Cadastar entradas e saidas', () => {
     beforeEach(() => {
         cy.visit('https://devfinance-agilizei.netlify.app/');
     });
-    it('Cadastar uma entrada', () => {        
+    it('Cadastar uma entrada', () => {       
         cy.criarTransacao('Freela React', 3500);
+        cy.criarTransacao('Moto trilha', 13500);
 
-        cy.get('tbody tr .description').should('have.text', 'Freela React');
+        cy.get('tbody tr .description').should('contain.text', 'Freela React');
+        cy.get('tbody tr .description').should('contain.text', 'Moto trilha');
+        cy.get('#data-table tbody tr').should('have.length', 2);
     });
     
     it('Cadastrar uma saida', () => {
         cy.criarTransacao('Restaurante', -350);
+        cy.criarTransacao('Tv 60 polegadas', -5650);
         
-        cy.get('tbody tr .description').should('have.text', 'Restaurante');
+        cy.get('tbody tr .description').should('contain.text', 'Restaurante');
+        cy.get('tbody tr .description').should('contain.text', 'Tv 60 polegadas');
+        cy.get('#data-table tbody tr').should('have.length', 2);
     });
 
     it('Remover entrada e saida', () => {
-        cy.criarTransacao('Monitor', 1500);
-        cy.criarTransacao('Tv 60 polegadas', 4500);
+        cy.criarTransacao('Gurda roupa', 950);
+        cy.criarTransacao('Moto', 15000);
+        cy.criarTransacao('Pizza', -60);
  
-        cy.contains('.description', "Monitor")
+        cy.contains('.description', "Pizza")
             .parent()
             .find('img')
             .click();
         
-        cy.get('tbody tr').should('have.length', 1);
+        cy.get('tbody tr').should('have.length', 2);
+    });
+});
+
+describe('Cadastar sem prencher os campos obrigatórios', () => {
+    beforeEach(() => {
+        cy.visit('https://devfinance-agilizei.netlify.app/');
+    });
+    it('Cadastrar sem prencher a descrição', () => {
+        cy.criarTransacaoSemDescricao(2500);
+    });
+
+    it('Cadastrar sem prencher o valor', () => {
+        cy.criarTransacaoSemValor('Chevette');
+    });
+    it('Cadastrar sem prencher a data', () => {
+        cy.criarTransacaoSemData('Uno Turbo', 12500);
+    });
+
+    it('Cadastrar sem prenhcer nenhum campo', () => {
+        cy.criarTransacaoSemDado();
     });
 });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,3 +7,67 @@ Cypress.Commands.add('criarTransacao', (descricao, valor) => {
 
     cy.contains('button', 'Salvar').click();
 });
+
+Cypress.Commands.add('criarTransacaoSemDescricao', (valor) => {
+    cy.contains('Nova Transação').click();
+
+    cy.get('.input-group input[type="text"]').clear();
+    cy.get('.input-group input[type="number"]').type(valor);
+    cy.get('.input-group input[type="date"]').type("2023-01-01"); 
+
+    cy.contains('button', 'Salvar').click();
+
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal(`Por favor, preencha todos os campos`);
+    });
+
+    cy.contains('a', 'Cancelar').click();
+});
+
+Cypress.Commands.add('criarTransacaoSemValor', (descricao) => {
+    cy.contains('Nova Transação').click();
+
+    cy.get('.input-group input[type="text"]').type(descricao);
+    cy.get('.input-group input[type="number"]').clear();
+    cy.get('.input-group input[type="date"]').type("2023-01-01"); 
+
+    cy.contains('button', 'Salvar').click();
+
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal(`Por favor, preencha todos os campos`);
+    });
+
+    cy.contains('a', 'Cancelar').click();
+});
+
+Cypress.Commands.add('criarTransacaoSemData', (descricao, valor) => {
+    cy.contains('Nova Transação').click();
+
+    cy.get('.input-group input[type="text"]').type(descricao);
+    cy.get('.input-group input[type="number"]').type(valor);
+    cy.get('.input-group input[type="date"]').clear(); 
+
+    cy.contains('button', 'Salvar').click();
+
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal(`Por favor, preencha todos os campos`);
+    });
+
+    cy.contains('a', 'Cancelar').click();
+ });
+
+Cypress.Commands.add('criarTransacaoSemDado', () => {
+    cy.contains('Nova Transação').click();
+
+    cy.get('.input-group input[type="text"]').clear();
+    cy.get('.input-group input[type="number"]').clear();;
+    cy.get('.input-group input[type="date"]').clear(); 
+
+    cy.contains('button', 'Salvar').click();
+
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal(`Por favor, preencha todos os campos`);
+    });
+
+    cy.contains('a', 'Cancelar').click();
+});
